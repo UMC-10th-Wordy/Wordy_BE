15c15bb78eef0780716c8efdaf4403526350e690
SuKwang Lee <coptony212@gmail.com>
2026-07-30 06:54:45 +0900

    fix: 일지 상세 조회 Prisma relation 수정

--- src/modules/dailyentries/dailyentries.repository.ts
+++ src/modules/dailyentries/dailyentries.repository.ts
@@ -100,7 +100,7 @@ export const findEntryDetail = async (userId: string, dailyEntryId: string) => {
       reflectionSnapshots: {
         orderBy: { createdAt: "desc" },
         include: {
-          dailyPerformance: true,
+          dailyPerformances: true,
           reflectionTaskSnapshots: {
             include: {
               task: { include: { tag: true } },
