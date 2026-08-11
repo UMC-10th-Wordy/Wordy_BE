7c53135f84b58279abcf3d5376fb5d7dcdf35cec
Seojin <1020seojin@naver.com>
2026-08-12 07:36:45 +0900

    fix: migration 대소문자 수정

--- prisma/migrations/20260812073146_update_dashboard_kpi_and_focused_tags/migration.sql
+++ prisma/migrations/20260812073146_update_dashboard_kpi_and_focused_tags/migration.sql
@@ -1,7 +1,7 @@
 -- AlterTable
-ALTER TABLE `dashboard` ADD COLUMN `focused_tags` JSON NULL,
+ALTER TABLE `Dashboard` ADD COLUMN `focused_tags` JSON NULL,
     ADD COLUMN `key_achievement` TEXT NULL;
 
 -- AlterTable
-ALTER TABLE `dashboardkpi` ADD COLUMN `related_achievement` TEXT NULL,
+ALTER TABLE `DashboardKPI` ADD COLUMN `related_achievement` TEXT NULL,
     ADD COLUMN `tag_id` CHAR(36) NULL;
